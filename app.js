3b2f59d233db639482f1f672f603acf417cb5ec9
Steven Ott <StevenKarlOtt@gmail.com>
2020-08-25 02:59:24 -0400

    Got Pokemon on Battle field

--- app.js
+++ app.js
@@ -14,8 +14,15 @@ $.ajax({
     }).then(
         (data)=>{
             console.log(data)
-            $('#pokeName').html(data.name);
+            $('#pokemonImage').html(`<img src=${data.sprites.other["official-artwork"].front_default}>`)
+            $('#pokeName').html(data.name)
             $('#pokeType').html(data.types[0].type.name);
+            $('#pokeHealth').html(data.stats[0].base_stat)
+            $('#pokeAttack').html(data.stats[1].base_stat)
+            $('#pokeDefense').html(data.stats[2].base_stat)
+            $('#pokeSpecialAtt').html(data.stats[3].base_stat)
+            $('#pokeSpecialDef').html(data.stats[4].base_stat)
+            $('#pokeSpeed').html(data.stats[5].base_stat)
             pendingPokemon = data;
         },
         ()=>{
@@ -28,14 +35,11 @@ const assignPokemon = () => {
     let assignment = prompt("Is this for player: 1 or 2?");
     if(assignment == 1) {
         player1Pokemon = pendingPokemon
-      console.log("This is " + player1Pokemon.name)
-      console.log("this is " + player2Pokemon.name)
-      putPokemonOnBattlefield();
+      putPokemonOnBattlefield(assignment);
     }
     else if (assignment == 2) {
         player2Pokemon = pendingPokemon
-      console.log("This is " + player2Pokemon.name) 
-      console.log("This is " + player1Pokemon.name)
+      putPokemonOnBattlefield(assignment);
     }
     else {
         console.log("BAD CHOICE MR.")
@@ -43,8 +47,29 @@ const assignPokemon = () => {
 }
 
 const putPokemonOnBattlefield = (player) => {
-    
+
+    if(player == 1) {
+        $("#pokemonImage img").toggle({ effect: "scale", direction: "horizontal" })
+        $(`#player1 > info > h2`).text(player1Pokemon.name)
+        $("#player1 #pokeImage").html(`<img src=${player1Pokemon.sprites.other["official-artwork"].front_default}>`)
+        $("#player1").fadeIn("slow")
+        $("#player1 #pokeImage").effect("shake")
+        const pokeballPop = new Audio("./sounds/pokeballpop.mp3")
+        pokeballPop.play();
+    }
+    else {
+        $("#pokemonImage img").toggle({ effect: "scale", direction: "horizontal" })
+        $(`#player2 > info > h2`).text(player2Pokemon.name)
+        $("#player2 #pokeImage").html(`<img src=${player2Pokemon.sprites.other["official-artwork"].front_default}>`)
+        $("#player2").fadeIn("slow")
+        $("#player2 #pokeImage").effect("shake")
+        const pokeballPop = new Audio("./sounds/pokeballpop.mp3")
+        pokeballPop.play();
+    }
 }
+
+
+
 
 
 $('#confirm').on('click', assignPokemon)
